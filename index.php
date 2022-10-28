644f5ffdb52a8a6bd7ecad6fa66f038b73f85806
JhonLaden <116604658+JhonLaden@users.noreply.github.com>
2022-10-28 08:10:21 +0800

    log in page

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+    if(isset($_SESSION['logged-in'])){
+        header('location: admin/dashboard.php');
+    }else{
+        header('location: login/login.php');
+    }
+?>  
---
+++ login/login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel = "stylesheet" href = "../css/style.css">
+    <link rel = "stylesheet" href = "../css/circles.css">
+    <title>ice cream</title>
+</head>
+<body>
+    <div class="container">
+        <form class="form-container">
+            <div class="form-header">
+            <span class = "logo-container ">
+                <i class='bx bxl-drupal icon gradient'></i>
+            </span>
+            <div class="input-form">
+                <div class="user-form">
+                    <span class="icon-container"><i class='bx bxs-user gradient'></i></span>
+                    
+                    <input type="text" id = "username"  name= "username" required placeholder = "Username">
+                </div>
+                <div class="password-form">
+                <span class="icon-container"><i class='bx bxs-key gradient' ></i></span> 
+                    <input type="password" id = "password" name= "password" required placeholder = "Password">       
+                </div>
+            </div>
+
+            <input type="submit" name = "login-submit" class = "login-submit gradient" value = "LOGIN" >
+            </div>
+           
+            
+        
+            <div class="login-footer">
+                   <div class="circle-1 circle"></div>
+                   <div class="circle-2 circle"></div>
+                   <div class="circle-3 circle"></div>
+                   <div class="circle-4 circle"></div>
+                   <div class="circle-5 circle"></div>
+                   <div class="circle-6 circle"></div>
+                   <div class="circle-7 circle"></div>
+                   <div class="circle-8 circle"></div>
+                   <div class="circle-9 circle"></div>
+                   <div class="circle-10 circle"></div>
+                   <div class="circle-11 circle"></div>
+                   <div class="circle-12 circle"></div>
+                   <div class="circle-13 circle"></div>
+                   <div class="circle-14 circle"></div>
+                   <div class="circle-15 circle"></div>
+                   <div class="circle-16 circle"></div>
+                   <div class="circle-17 circle"></div>
+                   <div class="circle-18 circle"></div>
+                   <div class="circle-19 circle"></div>
+                   <div class="circle-20 circle"></div>
+                   <div class="circle-21 circle"></div>
+                   <div class="circle-22 circle"></div>
+                   <div class="circle-23 circle"></div>
+                   <div class="circle-24 circle"></div>
+                   <div class="circle-25 circle"></div>
+                   <div class="circle-26 circle"></div>
+                   <div class="circle-27 circle"></div>
+                   <div class="circle-28 circle"></div>
+
+
+
+            </div>
+            
+        </form>
+    </div>
+</body>
+</html>
